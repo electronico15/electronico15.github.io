395400cf004c29ad4ed54e315b3bc50d21226e66
electronico15 <74164067+electronico15@users.noreply.github.com>
2022-10-31 12:33:54 -0400

    Update index.js

--- nodejs-render/src/index.js
+++ nodejs-render/src/index.js
@@ -1,11 +1,12 @@
-const express = require('express')
-const server = express()
+http.createServer(function (request, response) {
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   // Send the response body as "Hello World"
+   response.end('Hello World\n');
+}).listen(8081);
 
-server.use((req, res) => {
-  res.send('Hello World!')
-})
-
-const PORT = process.env.PORT || 3000
-server.listen(PORT, () => {
-  console.log('Server listening on http://localhost:' + PORT)
-})
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
